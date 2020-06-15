759b1ba93647749cb23333c41d1c8adb159bb3d5
phetgary <56030176+phetgary@users.noreply.github.com>
2020-06-15 13:32:24 +0800

    create user model

--- AuthDemo/app.js
+++ AuthDemo/app.js
@@ -1,9 +1,28 @@
-var express = require("express");
-var mongoose = require("mongoose");
+var express                 = require("express"),
+    mongoose                = require("mongoose"),
+    passport                = require("passport"),
+    bodyparser              = require("body-parser"),
+    User                    = require("./models/user"),
+    LocalStrategy           = require("passport-local"),
+    passportLocalMongoose   = require("passport-local-mongoose");
+
 mongoose.connect("mongodb://localhost/auth_demo_app");
+
 
 var app = express();
 app.set("view engine", "ejs");
+
+app.use(require("express-session")({
+    secret: "Dodo is the best and cutest dog in the world",
+    resvae: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res){
     res.render("home");
