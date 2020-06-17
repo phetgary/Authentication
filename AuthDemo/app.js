872587c982480b5a961b3395c6e66047bc72affd
phetgary <56030176+phetgary@users.noreply.github.com>
2020-06-17 21:17:53 +0800

    add login route

--- AuthDemo/app.js
+++ AuthDemo/app.js
@@ -20,6 +20,7 @@ app.use(require("express-session")({
 
 app.use(passport.initialize());
 app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()))
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
@@ -49,6 +50,15 @@ app.post("/register", function(req, res){
             });
         }
     });
+});
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+app.post("/login", passport.authenticate("local",{
+    successRedirect: "/secret",
+    failureRedirect: "/login"
+}), function(req, res){
 });
 
 app.listen(3000, function(){
